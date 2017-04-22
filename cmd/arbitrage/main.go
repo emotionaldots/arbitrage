7a884595c3866370a62ce729e352d35c0cfdd172
emotionaldots <emotionaldots@pth>
2017-04-22 14:38:59 +0200

    fix torrent file permissions

--- cmd/arbitrage/main.go
+++ cmd/arbitrage/main.go
@@ -130,7 +130,7 @@ func (app *App) GetTorrentName(torrent []byte) (string, error) {
 }
 
 func (app *App) SaveTorrent(torrent []byte, path string) error {
-	return ioutil.WriteFile(path, torrent, 0x0600)
+	return ioutil.WriteFile(path, torrent, 0600)
 }
 
 type job struct {
